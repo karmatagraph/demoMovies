8c2f974dda57e66730fa9ccf4156d3009e199517
karma <karmatashi954@gmail.com>
2022-09-20 11:22:36 +0545

    add of basic table view for the movie list

--- MoviesDemoApp/ViewController.swift
+++ MoviesDemoApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
 
+    // MARK: - Private methods
+    private func setup() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .systemPink
+    }
 
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // navigate to the detail screen
+        print("movie \(indexPath.row) clicked")
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "movies \(indexPath.row)"
+        return cell
+    }
+    
 }
 
