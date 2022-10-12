0bb263d6837ac775b9001e6108c344fe5e670f93
karma <karmatashi954@gmail.com>
2022-10-12 12:37:02 +0545

    add of dummy collection view cell

---
+++ MoviesDemoApp/Modules/CollectionView/MovieList/CollectionViewCell/MovieListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieListCollectionViewCell.swift
+//  MoviesDemoApp
+//
+//  Created by karma on 9/30/22.
+//
+
+import UIKit
+
+class MovieListCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MoviesDemoApp/Modules/CollectionView/MovieList/MovListCollecViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MovListCollecViewController.swift
+//  MoviesDemoApp
+//
+//  Created by karma on 9/30/22.
+//
+
+import UIKit
+
+class MovListCollecViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    // MARK: - Private methods
+    private func setup() {
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        
+    }
+    
+    
+    
+    
+}
